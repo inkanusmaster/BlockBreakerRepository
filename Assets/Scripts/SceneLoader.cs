e0fe2588ec1fc62570cb8fde051bf8e3861a3741
inkanusmaster <wojcicki.m89@gmail.com>
2021-02-27 15:56:18 +0100

    Dodany level2
    testujemy commity i pushe

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-
     public void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
