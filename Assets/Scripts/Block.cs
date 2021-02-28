53692cc8aa1fcd9793e1d78572128b280de93a63
inkanusmaster <wojcicki.m89@gmail.com>
2021-03-01 00:29:07 +0100

    Zliczanie punktów
    Trochę niezrozumiałe... Napisałem na forum

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,18 +6,27 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
 
+    //Referencja do Level
+    Level level;
+
+    private void Start()
+    {
+        //Szukamy obiektu typu Level i przypisujemy go pod zmienną level.
+        //Zwraca pierwszy aktywny załadowany obiekt danego typu.
+        //Ten typ nie zwraca assetów, ani nieaktywnych obiektów, tylko aktywne GameObjecty.
+        //Ale po co? Ta funkcja działa wolno. Chyba jako ciekawostka to?
+        //Musi to być. Bez tego nie zlicza.
+        //Wydaje mi się, że wyszukuje ten pierwszy aktywny obiekt, tutaj Level
+        //i każdy blok odnosi się właśnie to tego samego Levelu
+        //i każdy blok przy tworzeniu robi dzięki temu inkrementację w tym samym Levelu
+        level = FindObjectOfType<Level>();
+
+        //Zliczamy. Przy tworzeniu każdego klocka wykonuje się metoda Start() i level.CountBreakableBlocks().
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //Na kolizji, zanim zniszczymy obiekt robimy PlayClipAtPoint()
-        //Tworzymy chwilowo AudioSource i wywołujemy metodę
-        //PlayClipAtPoint z parametrem breakSound
-        //A tam w Unity już pod to pole damy nasz sound.
-        //Podajemy tutaj jako drugi parametr pozycję na scenie, z której pochodzi dźwięk.
-
-        //OK. Chcemy słyszeć dźwięk tam gdzie mamy kamerę.
-        //Możnaby dać z pozycji klocka, ale jak on jest dalej to dźwięk byłby cichszy.
-        //Dlatego zamiast transform x i y czyli miejsca klocka robimy Camera.main.transform bo z kamery chcemy słuchać
-        //Tym właśnie odwołujemy się do pozycji kamery! Głównej kamery. Nie mamy teraz innych
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    //Serializujemy dla celów debugowych
+    [SerializeField] int breakableBlocks = 0;
+
+    //Metoda zliczająca bloki na planszy
+    //Za każdym razem gdy ta metoda będzie wykonana, dodajemy 1 blok do breakableBlocks
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+
+}
