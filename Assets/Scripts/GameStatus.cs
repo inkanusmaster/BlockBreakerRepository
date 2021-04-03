6c813853cda1f0567d4381736904afb7473bdd67
inkanusmaster <wojcicki.m89@gmail.com>
2021-04-03 21:30:37 +0200

    Singleton jest
    GameStatus robimy żeby był tylko jeden, ten co na początku powstaje. I jego aktualizujemy we wszystkich scenach.
    Każdy kolejny obiekt GameStatus jest usuwany jeśli tylko już jakiś istnieje.
    Robimy to w Awake(), który odpalany jest przed Start().

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-//Do punktacji na ekranie. Bo tam mamy TextMeshPro.
 using TMPro;
 
 public class GameStatus : MonoBehaviour
@@ -10,12 +8,22 @@ public class GameStatus : MonoBehaviour
     [Range(0.1f, 5f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 2;
     [SerializeField] int currentScore = 0;
-
-    //Nasza punktacja co będzie na ekranie
     [SerializeField] TextMeshProUGUI scoreText;
 
-    //Robimy Start jednak. 
-    //Będzie zawierał inicjalizację score.
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+
+        if (gameStatusCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     private void Start()
     {
         scoreText.text = currentScore.ToString();
