2aeb3cb67111a7605005f33404601b45195f1c65
inkanusmaster <furymistrz@gmail.com>
2021-05-03 23:47:18 +0200

    Zrobiony damage!
    Teraz przejście do tuningowania gry

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,9 +8,11 @@ public class Block : MonoBehaviour
     //Configuration parameters
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
-
-    //ustalamy ile razy maksymalnie można walnąć w blok zanim się zniszczy całkiem.
     [SerializeField] int maxHits;
+
+    //Robimy tablicę Spritów. Będziemy do niej wczytywać sprity zawierające poziomy zniszczeń bloku.
+    //Normalna rzecz. SerializedField poniewać inicjalizujemy to elegancko w inspektorze.
+    [SerializeField] Sprite[] hitSprites;
 
     //Cached references
     Level level;
@@ -19,7 +22,7 @@ public class Block : MonoBehaviour
 
     private void Start()
     {
-        timesHit = 0; //Inicjalizujemy timesHit jako 0. Dla każdego bloku tak będzie.
+        timesHit = 0;
         CountBreakableBlocks();
     }
 
@@ -41,13 +44,29 @@ public class Block : MonoBehaviour
         }
     }
 
-    private void HandleHit() //Refaktoryzacja. Obsługujemy uderzenie.
+    private void HandleHit()
     {
-        timesHit++; //Jak walniemy w breakable to zwiększamy timesHit o 1.
-        if (timesHit >= maxHits) //Jesli blok zostanie walnięty maksymalną ilość razy, jest niszczony. Dla bezpieczeństwa lepiej dać >=
+        timesHit++;
+        if (timesHit >= maxHits)
         {
             DestroyBlock();
         }
+        else
+        {
+            //Będziemy pokazywać kolejny sprite po uderzeniu. Refaktoryzacja!
+            ShowNextHitSprite();
+        }
+    }
+
+    //Trzeba będzie zmieniać sprite po uderzeniu!
+    private void ShowNextHitSprite()
+    {
+        //Indexujemy od zera, więc jak trafimy w blok to elementy tablicy spritów iterujemy od zera.
+        int spriteIndex = timesHit - 1;
+
+        //Jesteśmy w prefabie blok. Ustawiamy w jego zakładce Sprite Renderer jako aktualny sprite ten nadany z tablicy spritów.
+        //Proste
+        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
     }
 
     private void DestroyBlock()
